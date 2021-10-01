b3934668aab09249bde5cb3e202d8f4fd8467350
H3JILgaH4uk <h3jilgah4uk@gmail.com>
2021-10-01 14:43:14 +0500

    app.js moved to root src

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import { startButtonListener, restartButtonListener, timeListListener } from './app/game';
+import rainbow from './app/rainbow';
+import { boardCircle } from './app/circle';
+import * as focus from './app/focus';
